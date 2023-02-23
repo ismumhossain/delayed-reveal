841d53c149df001c654d4c0f4f7e16562a281ffe
ihossain-6 <ismumhossain@gmail.com>
2023-02-23 16:16:09 +0600

    Initial commit

--- src/DelayReveal.sol
+++ src/DelayReveal.sol
@@ -3,11 +3,29 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC1155DelayedReveal.sol";
 
-contract Contract is ERC1155DelayedReveal {
+error CanNotMintThatMuch();
+error CanNotMintMore();
+error NotStarted();
+
+contract DelayReveal is ERC1155DelayedReveal {
     constructor(
         string memory _name,
-        string memory _symbol,
-        address _royaltyRecipient,
-        uint128 _royaltyBps
-    ) ERC1155DelayedReveal(_name, _symbol, _royaltyRecipient, _royaltyBps) {}
+        string memory _symbol
+    ) ERC1155DelayedReveal(_name, _symbol, msg.sender, 0) {}
+
+    function verifyClaim(
+        address _claimer,
+        uint256 _tokenId,
+        uint256 _quantity
+    ) public view override {
+        if(_quantity > 5) {
+            revert CanNotMintThatMuch();
+        }
+        if(this.balanceOf(_claimer, _tokenId) == 5) {
+            revert CanNotMintMore();
+        }
+        if(block.timestamp != 1677146400) {
+            revert NotStarted();
+        }
+    }
 }
